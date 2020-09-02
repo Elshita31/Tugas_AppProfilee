b4a686af8b1eb8d2aaa3320268b16184919f080e
Elshita31 <elshitaindah@gmail.com>
2020-09-02 22:58:45 +0700

    first commit

---
+++ app/src/main/java/id/sch/smktelkommalang/myappprofile2/MainActivity.kt
@@ -0,0 +1,72 @@
+package id.sch.smktelkommalang.myappprofile2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnSave.setOnClickListener{validasiInput()}
+
+        setDataSpinnerGender()
+    }
+
+    private fun setDataSpinnerGender(){
+        val adapter = ArrayAdapter.createFromResource(this,
+                R.array.gender_list, android.R.layout.simple_spinner_item)
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinnerGender.adapter = adapter
+    }
+
+    private fun validasiInput() {
+        val namaInput = edtName.text.toString()
+        val emailInput = edtEmail.text.toString()
+        val telpInput = edtTelp.text.toString()
+        val alamatInput = edtAddress.text.toString()
+        val genderInput = spinnerGender.selectedItem.toString()
+
+        when{
+            namaInput.isEmpty() -> edtName.error = "Nama tidak boleh kosong"
+            genderInput.equals("Pilih Jenis Kelamin") -> ("Jenis Kelamin tidak boleh kosong")
+            emailInput.isEmpty() -> edtEmail.error = "Email tidak boleh kosong"
+            telpInput.isEmpty() -> edtTelp.error = "Telp tidak boleh kosong"
+            alamatInput.isEmpty() -> edtAddress.error = "Alamat tidak boleh kosong"
+
+            else -> {
+                Toast.makeText(this, "Navigasi ke halaman profil diri", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+
+    private fun navigasiKeProfilDiri(){
+        val intent = Intent(this, MyProfileActivity::class.java)
+        startActivity(intent)
+
+        var namaInput = edtName.text.toString()
+        var emailInput = edtEmail.text.toString()
+        var telpInput = edtTelp.text.toString()
+        var alamatInput = edtAddress.text.toString()
+        var genderInput = spinnerGender.selectedItem.toString()
+
+        val bundle = Bundle()
+        bundle.putString("nama", namaInput)
+        bundle.putString("gender", genderInput)
+        bundle.putString("email", emailInput)
+        bundle.putString("telp", telpInput)
+        bundle.putString("alamat", alamatInput)
+
+        intent.putExtras(bundle)
+
+        startActivity(intent)
+    }
+
+}
